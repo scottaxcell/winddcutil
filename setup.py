2f6b4e6be5335725f2d52f1366bf64d9d4397df2
Scott Axcell <scott.axcell@gmail.com>
2023-09-13 22:13:02 -0600

    update monitorcontrol install dependency

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version="0.1.0",
     packages=["winddcutil"],
     entry_points={"console_scripts": ["winddcutil = winddcutil.__main__:main"]},
-    install_requires=["monitorcontrol>=3.0.3"],
+    install_requires=[
+        "monitorcontrol @ git+https://github.com/scottaxcell/monitorcontrol.git@0befac81d29331fb4d062e45af23d4804e54bb55"
+    ],
 )
